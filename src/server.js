228b4123db1b733f9bf174e215aa905a1689facb
Pedro Lucas <pedrolucaslr01@gmail.com>
2022-11-16 14:51:34 -0300

    Alterando as estruturas de pastas

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config({ path: '.env' });
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
